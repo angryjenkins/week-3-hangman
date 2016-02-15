0191749d4087d26de51805168ded2ecbf531f0c6
@angryjenkins <matt@angryjenkins.com>
2016-02-15 16:53:50 -0500

    game renders, guessing works, not showing in solution, game not ending

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,47 +1,73 @@
+function starWarsHangman(){
+	var words = ["han solo","luke skywalker","tatooine","jedi","chewbacca","jakku","sith"]; //this is a wordlist of star wars-related answers.
+		solution = words[Math.floor(Math.random()*words.length)];
+		reveal = [];
+		guessed = [];
+		wrong = [];
+		right = [];
+		lives = 6;
+		
+		showLives = document.getElementById("lives");
+		showGame = document.getElementById("gameDisplay");
+		showGuessed = document.getElementById("guessed");
+		showLastGuess = document.getElementById("lastGuess");
+		showStatus = document.getElementById("gameStatus");
 
-var game = {};
- 	game.words = ["han solo","luke skywealker","tatooine"];//this is a wordlist of star wars-related answers.
-	game.solution = game.words[Math.floor(Math.random()*game.words.length)];
-	game.printAnswer = [];
-	game.right = [];
-	game.wrong = [];
-	game.lives = 6;
-	
+		loseAudio = new Audio('audio_file.mp3');
 
-//check if the letters in solutioin are in the alphabet, so people don't have to guess numbers 
 
-function renderGame(){
-	//check solution for special characters.
+		//check if the letters in solutioin are in the alphabet, so people don't have to guess numbers 
+
+	 	alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h','i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
+	        't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+
+	for(var i=0;i<solution.length;i++){
+
+		if (alphabet.indexOf(solution[i]) == -1){
+			reveal.push(solution[i]);
+		} else {
+			reveal.push("_");
+		}
+	}
+
+	//show t\he game display with spaces and hyphens.
+	showGame.innerHTML = reveal.join("");
+
 	console.log("Game Rendered!");
- 	var alphabet = [a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z];
 
-	for(var i=0;i<game.solution.length;i++){
+	//play the game
 
-		if (alphabet.indexOf(game.solution[i]) == -1){
-			//display a blank.
-		} else {
-			//display the letter.
-		}
-	}
-}
+	document.onkeyup = function(event) {
+		var guess = String.fromCharCode(event.keyCode).toLowerCase();
+		showLastGuess.innerHTML = guess;
 
-//play the game
-
-document.onkeyup = function(event) {
-	var guess = String.fromCharCode(event.keyCode).toLowerCase();
-	if(game.right.indexOf(guess) != -1 || game.wrong.indexOf(guess) != -1){
-		//answer has been rpeviously guessed. Notify player to try again.
-	} else {
-		//answer has not been guessed already. Resume the game.
-		if (game.solution.indexOf(guess) == -1){
-			game.lives--;
-			if(game.lives == 0){
-				
+		if(guessed.indexOf(guess) != -1){
+			showStatus.innerHTML = "You have already guessed " + guess +". Try again.";
+			//answer has been rpeviously guessed. Notify player to try again.
+		} else { 
+			//answer has not been guessed already. Resume the game.
+			showStatus.innerHTML = "";
+			guessed.push(guess);
+			showLastGuess.innerHTML = guess;
+			showGuessed.innerHTML = guessed;
+			if (solution.indexOf(guess) == -1){
+				//guess is not in solution. take a life, add guess to werong array. Continue 
+				lives--;
+				showLives.innerHTML = lives;
+				if(lives == 0){
+					showStatus.innerHTML = "You have run out of lives. The game is over.";
+					return;
+				} else {
+				//guess is in the solution. Reveal the solution with the correct guess.
+					
+				}
+		
+			} else {
+				//correct guess. Display letter in the  
 			}
-			//guess is not in solution. take a life, add guess to werong array.
-		} else {
-			//correct guess. Display letter in the game. 
 		}
 	}
 }
 
+window.onLoad = starWarsHangman();
